622fdde3236e640979a8340fc156e1993b27f52a
guangcai1011 <1763452998@qq.com>
2021-12-04 22:34:03 +0800

    完善配置

---
+++ src/test/java/com/java17pro/demo/Test.java
@@ -0,0 +1,16 @@
+package com.java17pro.demo;
+
+import java.util.Random;
+
+public class Test {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+//            System.out.println((int) (Math.random() * 9));
+            Random random = new Random();
+            System.out.println(random.nextInt(9));
+        }
+
+    }
+
+}
